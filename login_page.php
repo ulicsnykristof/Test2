e6be9acad5823108e5670a1d2cec183eb750d420
ulicsnykristof <ulicsny.kristof@gmail.com>
2019-06-09 16:25:48 +0200

    teszt commit

--- login_page.php
+++ login_page.php
@@ -19,7 +19,7 @@
     <!-- Login Navbar -->
     <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
       <a class="navbar-brand" href="login_page.php">
-        DMOFFICE
+        DMOFFICE ASD
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
